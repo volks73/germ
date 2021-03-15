c5528e4d9d5bf15483fb4bc294f550724c7b6d96
Christopher Field <cfield2@gmail.com>
2021-03-15 15:21:46 -0400

    Add initial CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "Generate asciinema cast files without recording")]
+struct Args {
+    /// Input file
+    #[structopt(short = "i", long = "input", value_name("FILE"), parse(from_os_str))]
+    input_file: Option<PathBuf>,
+
+    /// Output file, stdout if not present
+    #[structopt(short = "o", long = "output", value_name("FILE"), parse(from_os_str))]
+    output_file: Option<PathBuf>,
+
+    /// Input command
+    #[structopt(requires("outputs"))]
+    input: Option<String>,
+
+    /// Output from input command
+    #[structopt(min_values = 1)]
+    outputs: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::from_args();
+    println!("{:?}", args);
 }
