ea5a0a7beccdcc2bd505271959ae61d2637d23fc
Christopher Field <cfield2@gmail.com>
2021-03-16 10:33:27 -0400

    Remove logging and verbosity
    There is currently nothing to log or additional output to display. It is
    just getting in the way for the time being. Maybe I will add it back in
    at a later time when it is needed. I am just not sure how the logging
    will work with the STDIO streams.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
 use anyhow::Result;
-use clap_verbosity_flag::Verbosity;
-use env_logger::fmt::Color as LogColor;
-use env_logger::Builder;
-use log::Level;
 use serde::{Deserialize, Serialize};
 use serde_json;
 use std::env;
@@ -134,9 +130,6 @@ struct Event<'a>(f64, EventKind, &'a str);
 #[derive(Debug, StructOpt)]
 #[structopt(about = "Generate asciicast files without using asciinema's recording functionality")]
 struct Args {
-    #[structopt(flatten)]
-    verbose: Verbosity,
-
     /// The prompt to display before the command.
     #[structopt(short = "p", long = "prompt", default_value = "~$ ")]
     prompt: String,
@@ -176,32 +169,6 @@ where
 
 fn main() -> Result<()> {
     let args = Args::from_args();
-    if let Some(level) = args.verbose.log_level() {
-        Builder::new()
-            .format(|buf, record| {
-                // This implmentation for a format is copied from the default format implemented for the
-                // `env_logger` crate but modified to use a colon, `:`, to separate the level from the
-                // message and change the colors to match the previous colors used by the `loggerv` crate.
-                let mut level_style = buf.style();
-                let level = record.level();
-                match level {
-                    // Light Gray, or just Gray, is not a supported color for non-ANSI enabled Windows
-                    // consoles, so TRACE and DEBUG statements are differentiated by boldness but use the
-                    // same white color.
-                    Level::Trace => level_style.set_color(LogColor::White).set_bold(false),
-                    Level::Debug => level_style.set_color(LogColor::White).set_bold(true),
-                    Level::Info => level_style.set_color(LogColor::Green).set_bold(true),
-                    Level::Warn => level_style.set_color(LogColor::Yellow).set_bold(true),
-                    Level::Error => level_style.set_color(LogColor::Red).set_bold(true),
-                };
-                let write_level = write!(buf, "{:>5}: ", level_style.value(level));
-                let write_args = writeln!(buf, "{}", record.args());
-                write_level.and(write_args)
-            })
-            .filter(Some("asciicast-gen"), level.to_level_filter())
-            .filter(None, Level::Warn.to_level_filter())
-            .try_init()?;
-    }
     let mut start_delay = 0;
     let speed = 1;
     let commands: Vec<Command> = if let Some(input_file) = args.input_file {
