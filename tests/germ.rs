2c0f637b9244b7eb29ec39025bb7e1842205c76d
Christopher Field <cfield2@gmail.com>
2021-03-25 11:40:21 -0400

    Add test

--- tests/germ.rs
+++ tests/germ.rs
@@ -1,14 +1,8 @@
 use assert_cmd::Command;
 
-#[test]
-fn input_arg_with_one_outputs_arg() {
-    let mut cmd = Command::cargo_bin("germ").unwrap();
-    let assert = cmd
-        .env("SHELL", "/bin/sh")
-        .args(&["echo Hello World", "Hello World"])
-        .assert();
-    assert.success().stdout(
-        r#"{"version":2,"width":80,"height":24,"env":{"SHELL":"/bin/sh","TERM":"xterm-256color"}}
+const TEST_SHELL: &str = "/bin/sh";
+const TEST_TERM: &str = "xterm-256color";
+const HELLO_WORLD_OUTPUT: &str = r#"{"version":2,"width":80,"height":24,"env":{"SHELL":"/bin/sh","TERM":"xterm-256color"}}
 [0.0,"o","$ "]
 [0.75,"o","e"]
 [0.785,"o","c"]
@@ -29,6 +23,26 @@ fn input_arg_with_one_outputs_arg() {
 [2.16,"o","\r\n"]
 [2.16,"o","Hello World\r\n"]
 [3.16,"o",""]
-"#,
-    );
+"#;
+
+#[test]
+fn input_arg_with_no_outputs_arg() {
+    let mut cmd = Command::cargo_bin("germ").unwrap();
+    let assert = cmd
+        .env("SHELL", TEST_SHELL)
+        .env("TERM", TEST_TERM)
+        .arg("echo Hello World")
+        .assert();
+    assert.success().stdout(HELLO_WORLD_OUTPUT);
+}
+
+#[test]
+fn input_arg_with_one_outputs_arg() {
+    let mut cmd = Command::cargo_bin("germ").unwrap();
+    let assert = cmd
+        .env("SHELL", TEST_SHELL)
+        .env("TERM", TEST_TERM)
+        .args(&["echo Hello World", "Hello World"])
+        .assert();
+    assert.success().stdout(HELLO_WORLD_OUTPUT);
 }
